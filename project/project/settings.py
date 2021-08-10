3c5299ac8bb5c971dfae299c210aa820b7354985
Song-EunJu <jd06280@naver.com>
2021-08-10 11:15:13 +0900

    [ADD] 프론트 파일 연결

--- project/project/settings.py
+++ project/project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'beans.apps.BeansConfig',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +72,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'static')
+]
+# 현재 static 파일들이 어디에 있는지 확인
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'beans', 'static')
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
